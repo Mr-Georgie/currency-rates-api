539fc90d17463b5512dfd45af1d461668864ed18
supergizzy <george.isiguzo@yahoo.com>
2021-01-30 11:07:17 +0100

    edited Server.js port variable

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import app from "./index.js";
 
 // specify the port
-const port = 3000;
+const port = process.env.PORT|| '3000' ;
 
 // listen for errors
-app.listen(process.env.PORT || port, (err) => {
+app.listen(port, (err) => {
     if (err) throw err
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("Server running on port: " + port)
 })
